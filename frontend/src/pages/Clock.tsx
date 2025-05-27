50d893c89ebac69a868dc860c3867e91086371df
Itzel <itzelvargas2002@gmail.com>
2025-05-26 21:19:04 -0600

    Add pages routing

---
+++ frontend/src/pages/Clock.tsx
@@ -0,0 +1,11 @@
+
+function Clock() {
+
+  return (
+    <>  
+      <h1>Pomodoro / chronometer</h1>
+    </>
+  )
+}
+
+export default Clock
---
+++ frontend/src/pages/Proyects.tsx
@@ -0,0 +1,11 @@
+
+function Proyects() {
+
+  return (
+    <>  
+      <h1>Proyects</h1>
+    </>
+  )
+}
+
+export default Proyects
