6fd48cbb585f56726a5a749ae7b84cc37818e197
Itzel <itzelvargas2002@gmail.com>
2025-06-02 21:59:45 -0600

    Add modes and useDarkMode hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import { useDarkMode } from "./hooks/useDarkMode";
 
 function App() {
+  const [isDarkMode, toggleTheme] = useDarkMode();
 
   return (
-    <>  
+    <div className="w-screen bg-primary dark:bg-base dark:text-white">  
       <h1>Landing Page</h1>
-    </>
+      <button
+        onClick={toggleTheme}
+        className="px-4 py-2 border rounded dark:text-black"
+      >
+        Cambiar tema
+      </button>
+    </div>
   )
 }
 
