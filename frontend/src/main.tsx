1fa22a8e153091899dedcbe02ae4d920dd527988
Itzel <itzelvargas2002@gmail.com>
2025-06-02 18:49:59 -0600

    Refactor router, adding layout change

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,14 +8,29 @@ import Proyects from './pages/Proyects.tsx';
 import Tasks from './pages/Tasks.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import PrivateLayout from './layouts/PrivateLayout.tsx';
+import PublicLayout from './layouts/PublicLayout.tsx';
 
 const router = createBrowserRouter([
-  {path: '/', element: <App />},
-  {path: 'clock', element: <Clock />},
-  {path: 'dashboard', element: <Dashboard />},
-  {path: 'proyects', element: <Proyects />},
-  {path: 'tasks', element: <Tasks />},
+  {
+    path: '/',
+    element: <PublicLayout />,
+    children:[
+      { index: true, element: <App /> },
+    ]
+  },
+  {
+    path: '/',
+    element: <PrivateLayout />,
+    children: [
+      {path: 'clock', element: <Clock />},
+      {path: 'dashboard', element: <Dashboard />},
+      {path: 'proyects', element: <Proyects />},
+      {path: 'tasks', element: <Tasks />},
+    ]
+  },
   {path: "*", element: <NotFoundPage />},
+  
 ]);
 
 createRoot(document.getElementById('root')!).render(
