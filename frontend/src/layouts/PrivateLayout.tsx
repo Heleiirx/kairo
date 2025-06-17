5e5379579044dbd17e8af37ec0b39bae266e7bea
Itzel <itzelvargas2002@gmail.com>
2025-06-16 19:04:48 -0600

    Create public and private layouts

--- frontend/src/layouts/PrivateLayout.tsx
+++ frontend/src/layouts/PrivateLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import SideNavBar from '../components/SideNavBar';
+import SideNavBar from '../ui components/SideNavBar';
 
 export default function PrivateLayout() {
   return (
