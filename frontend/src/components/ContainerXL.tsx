6b3c1cc33bd8f10e068012770a453caee7a30595
Itzel <itzelvargas2002@gmail.com>
2025-06-16 19:03:54 -0600

    Create container XL

--- frontend/src/components/ContainerXL.tsx
+++ frontend/src/components/ContainerXL.tsx
@@ -7,7 +7,7 @@ interface ContainerXLProps {
 
 export default function ContainerXL({ children, className = '' }: ContainerXLProps) {
   return (
-    <div className={`w-screen bg-primary dark:bg-base dark:text-white h-lvh ${className}`}>
+    <div className={`w-full bg-primary dark:bg-base dark:text-white h-lvh ${className}`}>
       {children}
     </div>
   );
