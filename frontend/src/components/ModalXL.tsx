af9da362bcb61012d9213cd4c2d3b0f84c6194d1
Itzel <itzelvargas2002@gmail.com>
2025-06-19 19:22:15 -0600

    Edit modalXl adding shadow

--- frontend/src/components/ModalXL.tsx
+++ frontend/src/components/ModalXL.tsx
@@ -1,6 +1,6 @@
 export default function ModalXL({children}: {children?: React.ReactNode}) {
     return (
-        <div className="w-6xl h-80 min-h-9/10 bg-white rounded shadow-xl/30">
+        <div className="w-6xl h-80 min-h-9/10 bg-white rounded shadow-xl/30 relative">
             {children}
         </div>
     )
