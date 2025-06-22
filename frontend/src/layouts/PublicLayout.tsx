7ae07f537301b7b98a882522a925dcf0cb1ce8d0
Itzel <itzelvargas2002@gmail.com>
2025-06-22 16:14:33 -0600

    Add modal public layout

--- frontend/src/layouts/PublicLayout.tsx
+++ frontend/src/layouts/PublicLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import TopNavBar from '../ui components/TopNavBar';
+import ModalUserAuth from '../ui components/ModalUserAuth';
+import { AuthModalProvider } from '../context/AuthModalContext';
 
 export default function PublicLayout() {
   return (
-    <>
+    <AuthModalProvider>
       <TopNavBar />
       <main>
         <Outlet />
       </main>
-    </>
+      <ModalUserAuth />
+    </AuthModalProvider>
   );
 }
