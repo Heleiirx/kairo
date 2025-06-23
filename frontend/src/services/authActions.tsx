2cccb249ca9944610aeac581c721f1cb6d5d6092
Itzel <itzelvargas2002@gmail.com>
2025-06-22 19:54:43 -0600

    Update token and user on Auth

--- frontend/src/services/authActions.tsx
+++ frontend/src/services/authActions.tsx
@@ -1,6 +1,7 @@
 // src/routes/auth/authActions.ts
 import { redirect } from "react-router-dom";
 import api from "../services/api";
+import { useAuthStore } from "../store/authStore";
 
 // export async function loginAction({ request }: any) {
 //   const formData = await request.formData();
@@ -9,6 +10,7 @@ import api from "../services/api";
 
 //   try {
 //     await api.post("/login", { email, password });
+//     authStore.getState().setToken(mockToken);
 //     return redirect("/dashboard");
 //   } catch (error) {
 //     return { error: "Credenciales inválidas" };
@@ -22,6 +24,7 @@ import api from "../services/api";
 
 //   try {
 //     await api.post("/register", { email, password });
+//     useAuthStore.getState().login(token, user);
 //     return redirect("/dashboard");
 //   } catch (error) {
 //     return { error: "Error al crear la cuenta" };
@@ -34,10 +37,12 @@ export async function registerAction({ request }: any) {
   const email = formData.get("email");
   const password = formData.get("password");
 
+  useAuthStore.getState().login(email, password);
   console.log("Datos recibidos en signUp:", { email, password });
 
   // Solo para prueba: devuelve los datos a la vista
-  return { email, password, message: "Datos recibidos correctamente (test)" };
+  console.log( email, password,);
+  return redirect("/dashboard");
 }
 
 // src/routes/auth/authActions.ts
