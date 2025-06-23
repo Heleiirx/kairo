44f2880217e67734eb4b57b510094da22669e48f
gaboparra <geparra06@gmail.com>
2025-06-23 20:39:14 -0300

    lastname optional in schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    lastname: { type: String, required: true },
+    lastname: { type: String },
     email: {
       type: String,
       required: true,
