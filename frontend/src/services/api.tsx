2d31e635ac26480f32d001c899bcde603d8f1c10
Itzel <itzelvargas2002@gmail.com>
2025-06-24 17:06:58 -0600

    Remove withCredentials from api

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/",
-  withCredentials: true, // solo con cookies para auth
+  baseURL: "http://localhost:8080/api" // solo con cookies para auth
 });
 
 export default api;
