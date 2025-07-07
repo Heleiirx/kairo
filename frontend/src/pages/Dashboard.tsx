b7bedc05f9e391bed51f3449dc553a85b9b72be6
Itzel <itzelvargas2002@gmail.com>
2025-07-07 16:29:34 -0600

    Refactor Dashboard component to use updated user structure from authStore, displaying user's name instead of email

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,13 @@
 import { useAuthStore } from "../store/authStore"
 
 function Dashboard() {
-  const { user, logout} = useAuthStore();
+  const user = useAuthStore((state) => state.user);
+  const logout = useAuthStore((state) => state.logout);
 
   return (
     <>  
       <h1>Dashboard</h1>
-      <h2>Hola, {user?.email} </h2>
+      <h2>Hola, {user?.name} </h2>
       <button onClick={logout}>Cerrar sesión</button>
     </>
   )
