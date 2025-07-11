949d9c896a90fd10b6988004d77d7930a703bd8a
Itzel <itzelvargas2002@gmail.com>
2025-07-11 12:57:46 -0600

    Add ContainerXL wrapper in PrivateLayout for improved layout structure

--- frontend/src/layouts/PrivateLayout.tsx
+++ frontend/src/layouts/PrivateLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import SideNavBar from '../ui components/SideNavBar';
+import ContainerXL from '../components/ContainerXL';
 
 export default function PrivateLayout() {
   return (
+    <ContainerXL>
     <div className="flex">
       <SideNavBar />
       <main className="flex-1 p-4">
         <Outlet />
       </main>
     </div>
+    </ContainerXL>
   );
 }
