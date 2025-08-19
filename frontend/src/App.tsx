df55a5898054e7d19d825cb2b1e302cd475f8ea7
Itzel <itzelvargas2002@gmail.com>
2025-08-19 14:52:46 -0600

    Add title landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,10 @@ function App() {
   return (
     // Edit class 
     <ContainerXL className="flex flex-col items-center justify-center">  
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-4">
+        <h2 className="text-lg">Welcome to</h2>
+        <h1 className="text-6xl font-serif">Kairo</h1>
+        <p className="text-sm"> Kairo es una aplicación web de organización personal basada en la técnica Pomodoro y en la filosofía del tiempo oportuno (kairos). </p>
       </div>
     </ContainerXL>
   )
